a38b8f0d253676e57a5a694ace58348d9daecdd4
dfergusbrown <dfergusbrown@gmail.com>
2023-11-28 22:09:08 -0500

    Featured Carousel

---
+++ components/featuredCarousel/FeaturedCarousel.jsx
@@ -0,0 +1,32 @@
+import { Col, Container, Row } from "react-bootstrap";
+import AFItop100list from "./AFItop100list";
+import useApiRequest from "../useApiRequest";
+
+const FeaturedCarousel = () => {
+
+    const randomArray = []
+    for (let i = 0; randomArray.length < 3; i++) {
+        let newRandomSelection = Math.floor(Math.random() * 124);
+        if (!randomArray.includes(AFItop100list[newRandomSelection])) {
+            randomArray.push(AFItop100list[newRandomSelection])
+        }
+    }
+
+    return (
+        <Container className="fluid">
+            <Row className="carousel">
+                {randomArray.map(item => {
+                    const posterURL = useApiRequest(item)
+                    
+                    return (
+                        <Col>
+                            <img src={posterURL.apiData.Poster} key={item}/>
+                        </Col>
+                    )
+                })}
+            </Row>
+        </Container>
+    )
+}
+
+export default FeaturedCarousel;
---
+++ components/useApiRequest.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+
+const useApiRequest = title => {
+  const [apiData, setData] = useState([]);
+  const baseURL = 'http://www.omdbapi.com/?apikey='
+  const apiKey = '5c02e86e'
+
+  useEffect(() => {
+      fetch(baseURL + apiKey +'&t='+ title)
+      .then(response => response.json())
+      .then(data => {
+        setData(data)
+        console.log(data)
+      })
+      .catch(error => console.log(error))
+  }, []);
+
+  return { apiData };
+};
+
+export default useApiRequest;

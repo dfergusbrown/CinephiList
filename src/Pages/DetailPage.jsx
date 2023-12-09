024ecd03e7b976173e4596db181fd2cc1085b122
Logan Hall <info.tech.logan@gmail.com>
2023-12-09 17:18:27 -0500

    Added details and plot and moved poster

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -1,19 +1,39 @@
 import useApiRequest from "../components/useApiRequest"
-import { Container } from "react-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
 import { useLocation } from "react-router-dom"
 
-const DetailPage = (props) => { 
+const DetailPage = (props) => {
     const location = useLocation()
-    
+
     console.log(location.state)
     const detailInfo = useApiRequest(location.state)
 
-    
-    return(
-        <Container>
-            <div>detail Page </div>
-            <img src={detailInfo.apiData.Poster} />
-        </Container>
+    const details = { "Title": "Title", "Released": "Released", "BoxOffice": "Box Office Earnings", "Director": "Director(s)", "Genre": "Genre", "Rated": "Rating", "Runtime": "Runtime" };
+
+    console.log(detailInfo);
+    return (
+        <Container id="detailsPage">
+            <div id="detailTitle">{`${detailInfo.apiData.Title} (${detailInfo.apiData.Year})`}</div>
+            <Row>
+                <Col>
+                    <img id="detailImage" src={detailInfo.apiData.Poster} />
+                </Col>
+                <Col id="detailsListCol">
+                    {Object.keys(details).map((key) => {
+                        return (
+                            <>
+                                <div id="detailsList">{`${details[key]}: ${detailInfo.apiData[key]}`}</div>
+                            </>
+                        )
+                    })}
+                    <div><Button variant="outline-secondary">Add To Collection</Button></div>
+                    <div><Button variant="outline-success">I Want This Movie!</Button></div>
+                </Col>
+            </Row>
+            <Row id="plot">
+                {`${detailInfo.apiData.Plot}`}
+            </Row>
+        </Container >
     )
 }
 

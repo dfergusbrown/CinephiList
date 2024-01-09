81966d2bd8ba1e6c6feafac51f9b4d97224d4866
Nate Rollins <nater9ster@gmail.com>
2024-01-09 10:41:01 -0500

    Detail Page

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -1,20 +1,33 @@
-import useApiRequest from "../components/useApiRequest"
-import { Container } from "react-bootstrap"
-import { useLocation } from "react-router-dom"
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import useApiRequest from "../components/useApiRequest";
 
-const DetailPage = (props) => { 
-    const location = useLocation()
-    
-    console.log(location.state)
-    const detailInfo = useApiRequest(location.state)
+const DetailPage = (props) => {
+  const location = useLocation();
+  const detailInfo = useApiRequest(location.state);
 
-    
-    return(
-        <Container>
-            <div>detail Page </div>
-            <img src={detailInfo.apiData.Poster} />
-        </Container>
-    )
-}
+  if (!detailInfo.apiData) {
+    // Data is not yet available, you can render a loading spinner or message here
+    return <div>Loading...</div>;
+  }
 
-export default DetailPage
+  return (
+    <Container>
+      <div>detail Page </div>
+      <img src={detailInfo.apiData.Poster} />
+
+      {/* Add buttons */}
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        <Button variant="primary" size="lg" style={{ marginRight: "10px" }}>
+          Add to My Collection
+        </Button>
+        <Button variant="success" size="lg">
+          Movies I Want!
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default DetailPage;
